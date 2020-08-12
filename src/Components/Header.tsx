80ff2d1e35db7ab16a727f2a8bddfd89269aaf02
User <uzairbangee@gmail.com>
2020-08-12 16:51:07 +0500

    home link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     title: {
@@ -22,9 +23,11 @@ const Header = () => {
         <Fragment>
             <AppBar position="static" className={classes.head}>
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                    Quiz App
-                    </Typography>
+                    <Link to={`/quiz/${props.ids}/${props.mode}`} style={{textDecoration: 'none'}}>
+                        <Typography variant="h6" className={classes.title}>
+                        Quiz App
+                        </Typography>
+                    </Link>
                 </Toolbar>
             </AppBar>
         </Fragment>
