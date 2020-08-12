766bf0e1cf24d08d71b7144f7d0b53c00a375d8c
User <uzairbangee@gmail.com>
2020-08-12 22:41:42 +0500

    PWA implemented

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -26,12 +26,13 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(
       process.env.PUBLIC_URL,
       window.location.href
     );
+
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -59,6 +60,9 @@ export function register(config?: Config) {
         registerValidSW(swUrl, config);
       }
     });
+  }
+  else{
+    console.log('Not Found')
   }
 }
 
